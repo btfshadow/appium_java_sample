65379697bbea076b1d2dd69f18b19775f30b0734
btfshadow <thiagosgoncalves7@gmail.com>
2019-09-16 17:05:01 -0300

    Delete build-android.sh

--- build-android.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd ../vv-viamais-android-app-vendedor
-git pull
-./gradlew assembleHomolog
-cd ../vv-viamais-qa
-cp ../vv-viamais-android-app-vendedor/app/build/outputs/apk/homolog/app-homolog.apk artifacts/app-ViaMais.apk
